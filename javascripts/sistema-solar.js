07a177a7bdea0fe0fe6f38c676e98c49fce46460
Felipe de Morais <felipejpa15@gmail.com>
2014-08-10 18:40:00 -0300

    ajustando o a translação da lua em torno da terra, adicionando iluminação, adicionando função de resize do canvas

--- javascripts/sistema-solar.js
+++ javascripts/sistema-solar.js
@@ -5,11 +5,30 @@ var fovy = 75,
     near = 1,
     far = 300;
 
+//Criando a cena
 var cena = new THREE.Scene();
 
+// Criando e posicionando a câmera
 var camera = new THREE.PerspectiveCamera(fovy, aspectRatio, near, far);
 camera.position.set(75, 75, 75);
 camera.lookAt(ORIGEM);
+
+// Criando a luz ambiente e adicionando a cena
+// var light = new THREE.AmbientLight(0x404040);
+// light.name = 'Luz Ambiente';
+// cena.add(light);
+
+// create a point light
+var pointLight =
+  new THREE.PointLight(0xFFFFFF);
+
+// set its position
+pointLight.position.x = 130;
+pointLight.position.y = 60;
+pointLight.position.z = 50;
+
+// add to the scene
+cena.add(pointLight);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -18,23 +37,26 @@ main_content.appendChild(renderer.domElement);
 
 function criarSol(objeto) {
     var geometriaSol = new THREE.SphereGeometry(20, 20, 20),
-        materialSol = new THREE.MeshBasicMaterial({color: 0xD6D637}),
+        materialSol = new THREE.MeshPhongMaterial({color: 0xD6D637, ambient: 0xD6D637, specular: 0x050505, shininess: 100}),
         sol = new THREE.Mesh(geometriaSol, materialSol);
 
     sol.add(objeto);
+    sol.name = 'Sol';
 
     cena.add(sol);
 
     return sol;
 }
 
+
 function criarTerra(objeto) {
     var geometriaTerra = new THREE.SphereGeometry(5, 20, 20),
-    materialTerra = new THREE.MeshBasicMaterial({color: 0x0000FF}),
-    terra = new THREE.Mesh(geometriaTerra, materialTerra);
+        materialTerra = new THREE.MeshLambertMaterial({color: 0x0000FF}),
+        terra = new THREE.Mesh(geometriaTerra, materialTerra);
 
     terra.add(objeto);
     terra.translateZ(50);
+    terra.name = 'Terra';
 
     cena.add(terra);
 
@@ -43,12 +65,21 @@ function criarTerra(objeto) {
 
 function criarLua() {
     var geometriaLua = new THREE.SphereGeometry(1, 20, 20),
-        materialLua = new THREE.MeshBasicMaterial({color: 0x909090}),
+        materialLua = new THREE.MeshLambertMaterial({color: 0x909090}),
         lua = new THREE.Mesh(geometriaLua, materialLua);
+    lua.name = 'Lua';
+    lua.translateZ(10);
 
-        lua.translateZ(60);
+    cena.add(lua);
 
-        cena.add(lua);
+    return lua;
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
 var lua = criarLua();
@@ -60,6 +91,8 @@ function render(){
 
     requestAnimationFrame(render);
     renderer.render(cena, camera);
+
+    window.addEventListener( 'resize', onWindowResize, false );
 }
 
 render();
